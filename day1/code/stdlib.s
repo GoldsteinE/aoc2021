0d63d367f1c81de6f3abe00c62f0f09c6c6d7398
Goldstein <root@goldstein.rs>
2021-12-01 19:48:36 +0300

    remove unused parts of asm stdlib

--- day1/code/stdlib.s
+++ day1/code/stdlib.s
@@ -1,53 +1,17 @@
-.ifndef included_stdlib
-included_stdlib:
-
 .intel_syntax noprefix
 
 .include "constants.s"
 .include "strings.s"
-.include "lists.s"
 
 .section .data
 printn_buf: .ds.b 64
 
 .section .text
 
-# Choose between first and second part of the task
-.macro chooseimpl reg, first, second
-	mov \reg, offset \first
-	mov rax, [rsp]
-	mov rbx, 1
-	cmp rax, rbx
-	je .Lchooseimpl.end
-	mov \reg, offset \second
-	.Lchooseimpl.end:
-.endm
-
-.macro allow_tests
-	.ifdef testing_included
-		jmp run_tests
-	.endif
-.endm
-
 .macro exit code
 	mov rax, SYS_EXIT
 	mov rdi, \code
 	syscall
-.endm
-
-.macro print name
-	mov rdi, offset \name
-	mov rsi, \name\()_len
-	call putsn
-.endm
-
-.macro printn reg
-	mov rdi, \reg
-	mov rsi, offset printn_buf
-	call itos
-	mov rdi, offset printn_buf
-	mov rsi, rax
-	call putsn
 .endm
 
 .macro trysyscall
@@ -77,18 +41,6 @@ fputsn:
 .Lfputsn.end:
 	ret
 
-# type: (int fd, char* buf) -> void
-.global fputs
-fputs:
-	push rdi
-	push rsi
-	mov rdi, rsi
-	call strlen
-	pop rsi
-	pop rdi
-	mov rdx, rax
-	jmp fputsn
-
 # type: (char* buf, int n) -> void
 .global putsn
 putsn:
@@ -96,13 +48,6 @@ putsn:
 	mov rsi, rdi
 	mov rdi, STDOUT
 	jmp fputsn
-
-# type: (char* buf) -> void
-.global puts
-puts:
-	mov rsi, rdi
-	mov rdi, STDOUT
-	jmp fputs
 
 # type: (int fd) -> void
 .global fputnl
@@ -141,5 +86,3 @@ readline:
 	sub rsi, rbx
 	mov rbx, rsi
 	ret
-
-.endif

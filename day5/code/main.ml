9428058d32baff60be60a723b22edb9d3a2ca8e5
Goldstein <root@goldstein.rs>
2021-12-05 16:17:29 +0300

    some style fixes to day 5

--- day5/code/main.ml
+++ day5/code/main.ml
@@ -4,8 +4,8 @@ open Printf
 open Stdlib
 open Sys
 
-let table = Hashtbl.create 100000;;
-let part = if Sys.argv.(1) = "1" then 1 else 2;;
+let table = create 100000;;
+let part = if argv.(1) = "1" then 1 else 2;;
 
 let continue = ref true;;
 while !continue do
@@ -13,15 +13,15 @@ while !continue do
         let (x1, y1, x2, y2) = scanf "%d,%d -> %d,%d\n" (fun x1 y1 x2 y2 -> (x1, y1, x2, y2)) in (
             if x1 = x2 then
                 for y = (min y1 y2) to (max y1 y2) do
-                    match Hashtbl.find_opt table (x1, y) with
-                    | Some count -> Hashtbl.replace table (x1, y) (count + 1)
-                    | None -> Hashtbl.replace table (x1, y) 1
+                    match find_opt table (x1, y) with
+                    | Some count -> replace table (x1, y) (count + 1)
+                    | None -> replace table (x1, y) 1
                 done
             else if y1 = y2 then
                 for x = (min x1 x2) to (max x1 x2) do
-                    match Hashtbl.find_opt table (x, y1) with
-                    | Some count -> Hashtbl.replace table (x, y1) (count + 1)
-                    | None -> Hashtbl.replace table (x, y1) 1
+                    match find_opt table (x, y1) with
+                    | Some count -> replace table (x, y1) (count + 1)
+                    | None -> replace table (x, y1) 1
                 done
             else if part = 2 then
                 let (x1, y1, x2, y2) = if x1 < x2 then
@@ -31,21 +31,21 @@ while !continue do
                 in
                 if y1 < y2 then
                     for shift = 0 to y2 - y1 do
-                        match Hashtbl.find_opt table (x1 + shift, y1 + shift) with
-                        | Some count -> Hashtbl.replace table (x1 + shift, y1 + shift) (count + 1)
-                        | None -> Hashtbl.replace table (x1 + shift, y1 + shift) 1
+                        match find_opt table (x1 + shift, y1 + shift) with
+                        | Some count -> replace table (x1 + shift, y1 + shift) (count + 1)
+                        | None -> replace table (x1 + shift, y1 + shift) 1
                     done
                 else
                     for shift = 0 to y1 - y2 do
-                        match Hashtbl.find_opt table (x2 - shift, y2 + shift) with
-                        | Some count -> Hashtbl.replace table (x2 - shift, y2 + shift) (count + 1)
-                        | None -> Hashtbl.replace table (x2 - shift, y2 + shift) 1
+                        match find_opt table (x2 - shift, y2 + shift) with
+                        | Some count -> replace table (x2 - shift, y2 + shift) (count + 1)
+                        | None -> replace table (x2 - shift, y2 + shift) 1
                     done
         );
     with
         End_of_file -> continue := false;
 done;;
 
-let ans = Hashtbl.fold (fun (_, _) c a -> if c > 1 then a + 1 else a) table 0;;
+let ans = fold (fun (_, _) c a -> if c > 1 then a + 1 else a) table 0;;
 
-Printf.printf "%d\n" ans;;
+printf "%d\n" ans;;
